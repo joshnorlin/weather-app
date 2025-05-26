397149cef6ce67ce1db48253034e040d6d738b8c
joshnorlin <169235170+joshnorlin@users.noreply.github.com>
2025-05-25 21:02:14 -0600

    Removed debugging statements

--- components/CityDisplay.tsx
+++ components/CityDisplay.tsx
@@ -2,8 +2,6 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 import parseStateAbbreviations from '../utils/parseStateAbbreviations';
 
 export default function CityList({ city, onPress }) {
-  console.log(city.admin_name);
-  console.log(city.iso2);
   return (
     <View>
       <Pressable onPress={onPress}>
--- utils/callWeatherApi.ts
+++ utils/callWeatherApi.ts
@@ -1,7 +1,4 @@
 export default async function callWeatherApi(cityData) {
-
-  console.log('Call Weather API Function results: ', cityData);
-
   const lat = cityData.lat;
   const lon = cityData.lng;
   const apiKey = "64dd29ae64329d866cdac373ca28f5fc";
@@ -12,7 +9,6 @@ export default async function callWeatherApi(cityData) {
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
     );
     const json = await response.json();
-    console.log(json);
     return json;
   } catch (error) {
     console.error(error);
